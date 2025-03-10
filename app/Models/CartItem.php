b4c08a7eae1868995d6bc50b2cb96bca079cb994
Eduardo Coello <eduardongua@hotmail.com>
2025-03-10 13:16:52 -0700

    muestreo de los elementos dentro del carrito y añadi la relacion hasOneThrough para acceder a los productos a traves del carrito como un "cart item"

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -2,10 +2,31 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Product;
+use App\Models\ProductVariant;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOneThrough;
 
 class CartItem extends Model
 {
     use HasFactory;
+    
+    // La relación hasOneThrough permite acceder a Product a través de ProductVariant.
+    public function product(): HasOneThrough
+    {
+        return $this->hasOneThrough(
+            Product::class,          // Modelo final (Product)
+            ProductVariant::class,   // Modelo intermedio (ProductVariant)
+            'id',                    // Clave foránea en el modelo intermedio (ProductVariant)
+            'id',                    // Clave local en el modelo final (Product)
+            'product_variant_id',    // Clave local en el modelo actual (CartItem)
+            'product_id'             // Clave foránea en el modelo intermedio (ProductVariant)
+        );
+    }
+
+    public function variant()
+    {
+        return $this->belongsTo(ProductVariant::class, 'product_variant_id');
+    }
 }
