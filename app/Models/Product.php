be519477627fd599dcd73797aca044bc66f391a5
Eduardo Coello <eduardongua@hotmail.com>
2025-03-10 09:19:08 -0700

    cambios

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use Money\Money;
+use Money\Currency;
 use App\Models\Image;
+use Livewire\Attribute;
 use App\Models\ProductVariant;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -13,9 +17,23 @@ class Product extends Model
     /** @use HasFactory<\Database\Factories\ProductFactory> */
     use HasFactory;
 
+    protected function price(): Attribute
+    {
+        return Attribute::make(
+            get: function(int $value) {
+                return new Money($value, new Currency('MXN'));
+            }
+        );
+    }
+
     public function variants(): HasMany
     {
         return $this->hasMany(ProductVariant::class);
+    }
+
+    public function image(): HasOne
+    {
+        return $this->hasOne(Image::class)->ofMany('featured', 'max');
     }
 
     public function images(): HasMany
--- resources/views/livewire/store-front.blade.php
+++ resources/views/livewire/store-front.blade.php
@@ -1,3 +1,12 @@
-<div>
-    {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
+<div class="grid grid-cols-4 gap-4 mt-12">
+    @foreach ($this->products as $product)
+        <div>
+            <img src="{{ $product->image->path }}" alt="">
+            <h2 class="font-medium text-lg">{{ $product->name }}</h2>
+            <span class="text-gray-700 text-sm">{{ $product->price }}</span>
+        </div>
+    @endforeach
+    @php
+        echo app()->getLocale();
+    @endphp
 </div>
